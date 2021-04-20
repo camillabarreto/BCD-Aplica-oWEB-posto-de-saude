1649552916b68003049b6baed2b00497fb806c0d
camillabarreto <camillabarretodesousa@gmail.com>
2021-04-20 13:57:38 -0300

    adicionado descriçao no formulario de busca

--- projeto-bcd-02/forms/paciente.py
+++ projeto-bcd-02/forms/paciente.py
@@ -2,5 +2,5 @@ from flask_wtf import FlaskForm
 from wtforms import SubmitField, StringField, validators
 
 class PacienteForm(FlaskForm):
-    CPF = StringField('CPF')
+    CPF = StringField('CPF', [validators.DataRequired()], description='Digite a sequência numérica sem os separadores', render_kw={"placeholder": "11122233344"})
     btnAtualizar = SubmitField('Buscar')
