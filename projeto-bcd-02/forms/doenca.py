c4701726d2617697a30da09dcb4a044e5667cba0
camillabarreto <camillabarretodesousa@gmail.com>
2021-04-20 13:58:52 -0300

    adicionado descriçao no formulario de doenca

--- projeto-bcd-02/forms/doenca.py
+++ projeto-bcd-02/forms/doenca.py
@@ -2,5 +2,5 @@ from flask_wtf import FlaskForm
 from wtforms import SubmitField, IntegerField, validators
 
 class DoencaForm(FlaskForm):
-    idDoenca = IntegerField('idDoenca')
+    idDoenca = IntegerField('idDoenca', [validators.DataRequired()], description='Digite número identificador da doença')
     btnAtualizar = SubmitField('Buscar')
