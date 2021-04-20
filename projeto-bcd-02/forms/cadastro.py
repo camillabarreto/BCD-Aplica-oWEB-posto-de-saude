33762eb4c4fa55c67cfa1ef9282a65c21b6a825e
camillabarreto <camillabarretodesousa@gmail.com>
2021-04-20 13:43:31 -0300

    adicionado descriçao no formulario do cadastro

--- projeto-bcd-02/forms/cadastro.py
+++ projeto-bcd-02/forms/cadastro.py
@@ -2,9 +2,9 @@ from flask_wtf import FlaskForm
 from wtforms import SubmitField, StringField, DateField, validators
 
 class CadastroForm(FlaskForm):
-    cpf = StringField('CPF',[validators.DataRequired()])
-    nome = StringField('Nome',[validators.DataRequired()])
-    dataNasc = DateField('Data de nascimento',[validators.DataRequired()],format='%d/%m/%Y')
-    telefone = StringField('Telefone',[validators.DataRequired()])
-    endereco = StringField('Endereço',[validators.DataRequired()])
+    cpf = StringField('CPF',[validators.DataRequired()], description='Digite a sequência numérica sem os separadores', render_kw={"placeholder": "11122233344"})
+    nome = StringField('Nome',[validators.DataRequired()], render_kw={"placeholder": "Nome completo"})
+    dataNasc = DateField('Data de nascimento',[validators.DataRequired()],format='%d/%m/%Y', render_kw={"placeholder": "dd/mm/yyyy"})
+    telefone = StringField('Telefone',[validators.DataRequired()], description='Digite a sequência numérica sem os separadores', render_kw={"placeholder": "4833335555"})
+    endereco = StringField('Endereço',[validators.DataRequired()], description='Informe sua cidade', render_kw={"placeholder": "Cidade"})
     btnAtualizar = SubmitField('Confirmar')
